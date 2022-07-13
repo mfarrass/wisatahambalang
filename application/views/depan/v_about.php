aacd85f123cbb306377fe252698498c9f46cc7b4
Muhamad Farras <mfarras196@gmail.com>
2022-07-14 05:24:38 +0700

    second commit

---
+++ application/views/depan/v_about.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About</title>
+    <link rel="shorcut icon" href="<?php echo base_url().'theme/images/icon.png'?>">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.min.css'?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/font-awesome.min.css'?>">
+    <!-- Simple Line Font -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/simple-line-icons.css'?>">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/slick-theme.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.min.css'?>">
+    <!-- Main CSS -->
+    <link href="<?php echo base_url().'theme/css/style.css'?>" rel="stylesheet">
+</head>
+
+<body>
+     <div style="position:fixed;left:20px;bottom:20px;z-index:999999;">
+<a href="https://api.whatsapp.com/send?phone=+6285100257920&text=Halo">
+<button style="background:#32C03C;vertical-align:center;height:36px;border-radius:5px;">
+<img src="https://web.whatsapp.com/img/favicon/1x/favicon.png"> Whatsapp Kami</button></a>
+</div>
+    <!--============================= HEADER =============================-->
+    <div class="header-topbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-md-9">
+                    <div class="header-top_address">
+                        <div class="header-top_list">
+                            <span class="icon-phone"></span>08123456789
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-envelope-open"></span>infohambalang@.gmail.com
+                        </div>
+                        <div class="header-top_list">
+                            <span class="icon-location-pin"></span>Hambalang, Kec. Citeureup, Bogor
+                        </div>
+                    </div>
+                    <div class="header-top_login2">
+                        <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-4 col-md-3">
+                    <div class="header-top_login mr-sm-3">
+                        <a href="<?php echo site_url('contact');?>">Hubungi Kami</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div data-toggle="affix">
+        <div class="container nav-menu2">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar2 navbar-toggleable-md navbar-light bg-faded">
+                        <button class="navbar-toggler navbar-toggler2 navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                            <span class="icon-menu"></span>
+                        </button>
+                        <a href="<?php echo site_url('');?>" class="navbar-brand nav-brand2 -mt-2"><img class="img img-responsive" width="110px;" src="<?php echo base_url().'theme/images/logo-hambalang-dark.png'?>"></a>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('');?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="<?php echo site_url('about');?>">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('guru');?>">Desa</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('siswa');?>">Penginapan</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('blog');?>">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('pengumuman');?>">Pengumuman</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('agenda');?>">Agenda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('download');?>">Download</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo site_url('galeri');?>">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
+                                </li>
+                             </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+      </div>
+      <section>
+</section>
+<!--//END HEADER -->
+<!--//END ABOUT IMAGE -->
+<!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Kata Sambutan</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima modi, incidunt qui placeat excepturi eos illum quia quos esse assumenda deleniti quod consequuntur dolor asperiores praesentium exercitationem? Quaerat temporibus accusamus provident iste cum dignissimos, eligendi asperiores. Iusto, rem repudiandae fugit pariatur.
+
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat tenetur quaerat voluptas totam accusamus molestias perferendis quae, rem modi expedita in eaque debitis ratione iste quas voluptatibus corrupti recusandae sed ut quos aliquam optio beatae perspiciatis laborum. Eligendi, sequi doloribus iure voluptatem ipsum eveniet, voluptas sit quod ipsam ab, voluptates eaque asperiores. Nulla quasi itaque, eligendi labore perferendis consequuntur deserunt.
+                </p>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta odio natus expedita dolorem, velit aliquam nobis laborum mollitia aspernatur blanditiis debitis in autem omnis at quam distinctio nam provident atque. Tempora nesciunt repellat corporis dignissimos, repudiandae architecto ad voluptate exercitationem incidunt quos aliquid pariatur error repellendus deserunt ex vero quod magnam consequuntur adipisci accusantium temporibus distinctio doloribus perspiciatis! Amet debitis itaque esse excepturi dolorem ut corrupti voluptatem praesentium dicta!
+                    <p>
+                    Lorem ipsum dolor sit amet.<p>
+                    (Kepala Desa Hambalang)     
+                </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="<?php echo base_url().'theme/images/sebuah-profile2.png'?>" class="img-fluid" alt="#">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END WELCOME TITLE -->
+    <!--============================= WELCOME TITLE =============================-->
+<section class="welcome_about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Moto</h2>
+                <p>
+                </p>
+                <p>“Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem modi numquam hic?”<p>
+ <h3>Judul</h3><p>
+1.	Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempore debitis dolores!<p>
+2.	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem modi numquam hic?<p>
+3.	Lorem ipsum dolor sit amet, consectetur adipisicing.
+
+                </p>
+                <p>
+<h3>Judul</h3><p>
+1.	Lorem ipsum dolor sit amet<p>
+2.	Lorem ipsum dolor sit amet<p>
+3.	Lorem ipsum 
+                    <p>
+<h3>Judul</h3><p>
+1.	Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem modi numquam hic?<p>
+2.	Lorem ipsum dolor sit amet consectetur.<p>
+3.	Lorem ipsum dolor sit, amet consectetur adipisicing elit.<p>
+    
+                </p>
+                </div>
+                <div class="col-md-5">
+                    <img src="<?php echo base_url().'theme/images/community.png'?>" class="img-fluid" alt="#">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END WELCOME TITLE -->
+    <!--============================= TESTIMONIAL =============================-->
+    <section class="testimonial">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Testimonial</h2>
+                </div>
+                <div class="col-md-12">
+                    <div class="single-item">
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate soluta illum dolores, quam harum vitae autem quis laudantium consequatur quidem nobis earum ullam minus fugiat ratione!</p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-1.png'?>" class="img-fluid" alt="#">
+                                <p><span>Hernandez Alvaro</span>Wisatawan</p>
+                            </div>
+                        </div>
+                        <div class="quote">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i>
+                            <p class="quote_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum in illo modi blanditiis commodi eveniet illum, quia totam natus aspernatur, quos, eos pariatur doloribus odio quam explicabo sint. Dolor, numquam! </p>
+                            <div class="testi-img_block">
+                                <img src="<?php echo base_url().'theme/images/student-2.png'?>" class="img-fluid" alt="#">
+                                <p><span>Elanoar Rigby</span>Warga Hambalang</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//END TESTIMONIAL -->
+    <!--============================= DETAILED CHART =============================-->
+    <div class="detailed_chart">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_1.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_guru;?></span> Guru
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_bottom chart_top">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_2.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_siswa;?></span> Siswa
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3 chart_top">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_3.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_files;?></span> Download
+                        </p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="chart-img">
+                        <img src="<?php echo base_url().'theme/images/chart-icon_4.png'?>" class="img-fluid" alt="chart_icon">
+                    </div>
+                    <div class="chart-text">
+                        <p><span class="counter"><?php echo $tot_agenda;?></span> Agenda</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--//END DETAILED CHART -->
+
+     <!--============================= FOOTER =============================-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="foot-logo">
+                    <a href="<?php echo site_url();?>">
+                        <img src="<?php echo base_url().'theme/images/logo-hambalang-putih.png'?>" class="img-fluid" width="220px;" alt="footer_logo" />
+                    </a>
+                    <p><?php echo date('Y');?> © HIMASI UNAS </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="sitemap">
+                        <h3>Menu Utama</h3>
+                        <ul>
+                            <li><a href="<?php echo site_url();?>">Home</a></li>
+                            <li><a href="<?php echo site_url('about');?>">About</a></li>
+                            <li><a href="<?php echo site_url('artikel');?>">Blog </a></li>
+                            <li><a href="<?php echo site_url('galeri');?>">Gallery</a></li>
+                            <li><a href="<?php echo site_url('contact');?>">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="sitemap">
+                      <h3>Lainnya</h3>
+                      <ul>
+                          <li><a href="<?php echo site_url('guru');?>">Desa</a></li>
+                          <li><a href="<?php echo site_url('siswa');?>">Penginapan </a></li>
+                          <li><a href="<?php echo site_url('pengumuman');?>">Pengumuman</a></li>
+                          <li><a href="<?php echo site_url('agenda');?>">Agenda</a></li>
+                          <li><a href="<?php echo site_url('download');?>">Download</a></li>
+                      </ul>
+                  </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="address">
+                        <h3>Hubungi Kami</h3>
+                        <p><span>Alamat: </span> Hambalang, Kec. Citeureup, Kabupaten Bogor, Jawa Barat 16810 </p>
+                        <p><span>Email: </span> infohambalang@.gmail.com
+                            <br> Phone : 08123456789 </p>
+                            <ul class="footer-social-icons">
+                                        <li><a href="https://m.facebook.com/mtstarbiyatulfalah.bantargebang"><i class="fa fa-facebook fa-fb" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.instagram.com/mtstarbiyatulfalah94/?igshid=1xyoqvuqccosg"><i class="fa fa-instagram fa-in" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.youtube.com/channel/UCcEDH5YygxNcE3CXB0covgg"><i class="fa fa-youtube fa-tw" aria-hidden="true"></i></a></li>
+                                    </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--//END FOOTER -->
+                <!-- jQuery, Bootstrap JS. -->
+                <script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tether.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+                <!-- Plugins -->
+                <script src="<?php echo base_url().'theme/js/slick.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/waypoints.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/counterup.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/owl.carousel.min.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/validate.js'?>"></script>
+                <script src="<?php echo base_url().'theme/js/tweetie.min.js'?>"></script>
+                <!-- Subscribe -->
+                <script src="<?php echo base_url().'theme/js/subscribe.js'?>"></script>
+                <!-- Script JS -->
+                <script src="<?php echo base_url().'theme/js/script.js'?>"></script>
+            </body>
+
+            </html>
